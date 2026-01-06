56afdb9a8f600ec7bfe577a93c00a66017647782
yatin sharma <yatins113@gmail.com>
2026-01-06 14:01:17 +0530

    first commit

---
+++ pattern1.py
@@ -0,0 +1,6 @@
+x = int(input())
+
+for i in range (0,x):
+    for j in range(0,x):
+        print("*", end=" ")
+    print()
---
+++ pattern4.py
@@ -0,0 +1,11 @@
+def function(x):
+    for i in range(0 , x):
+     for j in range(0 , i+1):
+        print(i, end=" ")
+     print()
+
+
+x = int(input())
+function(x)
+
+
